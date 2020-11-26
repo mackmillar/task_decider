4ee87902613e0b7199769dd656b7353bd08bcbbb
mack millar <mackmillar@gmail.com>
2020-11-26 15:18:50 +0000

    mvp completed

--- src/app_decider.py
+++ src/app_decider.py
@@ -1,2 +1,11 @@
 def app_decider(task1, task2):
-    return 'wash dishes'
+    
+    
+    if task1.description == "wash dishes" and task2.description == "cook dinner" or task1.description == "cook dinner" and task2.description == "wash dishes":
+        return 'wash dishes'
+    elif task1.description == 'clean windows' and task2.description == 'cook dinner' or task1.description == 'cook dinner' and task2.description == 'clean windows' :
+        return 'cook dinner'
+    elif task1.description == 'clean windows' and task2.description == 'wash dishes' or task1.description == 'wash dishes' and task2.description == 'clean windows' :
+        return "clean windows"
+
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,24 @@ from src.task import Task
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        self.wash_dishes = Task('wash the dishes', 5)
-        self.cook_dinner = Task('make some tasty food', 30)
-        self.clean_windows = Task('make them transparent', 20)
+        self.wash_dishes = Task('wash dishes', 5)
+        self.cook_dinner = Task('cook dinner', 30)
+        self.clean_windows = Task('clean windows', 20)
 
     def test_washdishes_over_cookdinner(self):
         self.assertEqual("wash dishes", app_decider(self.wash_dishes, self.cook_dinner))
+
+    def test_cookdinner_over_washdishes(self):
+        self.assertEqual("wash dishes", app_decider(self.cook_dinner, self.wash_dishes))
+
+    def test_cookdinner_over_cleanwindows(self):
+        self.assertEqual("cook dinner", app_decider(self.clean_windows, self.cook_dinner))
+
+    def test_cleanwindows_over_cookdinner(self):
+        self.assertEqual("cook dinner", app_decider(self.cook_dinner, self.clean_windows))
+
+    def test_cleanwindows_over_washdishes(self):
+        self.assertEqual("clean windows", app_decider(self.clean_windows, self.wash_dishes))
+
+    def test_cleanwindows_over_washdishes(self):
+        self.assertEqual("clean windows", app_decider(self.wash_dishes, self.clean_windows))
